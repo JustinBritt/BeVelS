16189f36be15b53593531cf9e721f1f8a6104f22
Justin Britt <britt@uwindsor.ca>
2022-12-05 17:47:19 -0500

    Implement interface

--- BeVelS.ECS.Systems.RenderSurfaces/Interfaces/IRenderSurfaceSystem.cs
+++ BeVelS.ECS.Systems.RenderSurfaces/Interfaces/IRenderSurfaceSystem.cs
@@ -1,17 +1,13 @@
 ﻿namespace BeVelS.ECS.Systems.RenderSurfaces.Interfaces
 {
-    using DefaultEcs.System;
-
     using BeVelS.ECS.Systems.GraphicsDevices.Interfaces;
+    using BeVelS.ECS.Systems.Interfaces;
     using BeVelS.ECS.Systems.Resolutions.Interfaces;
 
-    public interface IRenderSurfaceSystem : ISystem<float>
+    public interface IRenderSurfaceSystem : IPostUpdateSystem<float>
     {
         IGraphicsDeviceSystem GraphicsDeviceSystem { get; }
 
         IResolutionSystem ResolutionSystem { get; }
-
-        void PostUpdate(
-            float state);
     }
 }
